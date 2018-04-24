1034d352710c5be4aa1ca56a13d2a58f1ab10fa6
ftancula <ftancula@gmail.com>
2018-04-24 21:54:58 +0200

    another changes in user information and flat inforamtion page

--- www/assets/js/controllers.js
+++ www/assets/js/controllers.js
@@ -89,7 +89,39 @@ myApp.controllers = {
             };
         });
     },
+    ////////////////////////////
+    // Register new Owner Page Controller //
+    ////////////////////////////
+    registerPage: function (page) {
+        // Set button functionality to save a new flat.
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/register-new-owner"]'), function (element) {
+            element.onclick = function () {
+                var newTitle = page.querySelector('#name-input').value;
 
+                if (newTitle) {
+                    // If input title is not empty, create a new task.
+                    myApp.services.tasks.create(
+                        {
+                            title: newTitle,
+                            category: page.querySelector('#category-input').value,
+                            description: page.querySelector('#description-input').value,
+                            highlight: page.querySelector('#highlight-input').checked,
+                            urgent: page.querySelector('#urgent-input').checked
+                        }
+                    );
+
+                    // Set selected category to 'All', refresh and pop page.
+                    document.querySelector('#settings-list ons-list-item ons-radio').checked = true;
+                    document.querySelector('#settings-list ons-list-item').updateCategoryView();
+                    document.querySelector('#myNavigator').popPage();
+
+                } else {
+                    // Show alert if the input title is empty.
+                    ons.notification.alert('You must provide a task title.');
+                }
+            };
+        });
+    },
     ////////////////////////////////
     // Details Task Page Controller //
     ///////////////////////////////
