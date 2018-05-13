ccf745d11c5fb87f2a790cbe76ea914bb4883ac8
ftancula <ftancula@gmail.com>
2018-05-13 17:57:29 +0200

    added form for creating a new flat

--- www/assets/js/controllers.js
+++ www/assets/js/controllers.js
@@ -5,7 +5,7 @@
 myApp.controllers = {
 
     //Loader page
-    loaderPage: function (page) {
+    loaderPage: function(page) {
         if (myApp.services.common.token.get()) {
             myApp.services.common.token.check();
         } else {
@@ -18,48 +18,49 @@ myApp.controllers = {
     },
 
     //Login page
-    loginPage: function (page) {
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/login"]'), function (element) {
-            element.onclick = function () {
+    loginPage: function(page) {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/login"]'), function(element) {
+            element.onclick = function() {
                 ajax.sendForm(page, myApp.services.common.authorizeSuccess, myApp.services.common.authorizeFail);
             };
         });
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/register-new-owner"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/register-new-owner"]'), function(element) {
+            element.onclick = function() {
                 document.querySelector('#myNavigator').pushPage('html/auth/register_owner.html');
             };
         });
     },
 
-    registerPage: function (page) {
-        page.querySelectorAll('[component="button/register"]'), function (element) {
-            element.onclick = function () {
-                ajax.sendForm(page, myApp.services.common.authorizeSuccess, myApp.services.common.authorizeFail);
-            };//TODO make it work and properly register new user
-        };
+    registerPage: function(page) {
+        page.querySelectorAll('[component="button/register"]'),
+            function(element) {
+                element.onclick = function() {
+                    ajax.sendForm(page, myApp.services.common.authorizeSuccess, myApp.services.common.authorizeFail);
+                }; //TODO make it work and properly register new user
+            };
     },
 
     //Tabbar page
-    landlordTabbarPage: function (page) {
-        page.querySelector('[component="button/menu"]').onclick = function () {
+    landlordTabbarPage: function(page) {
+        page.querySelector('[component="button/menu"]').onclick = function() {
             document.querySelector('#landlordSplitter').left.toggle();
         };
     },
 
     //Menu page
-    menuPage: function (page) {
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/menu-item"]'), function (element) {
-            element.onclick = function () {
+    menuPage: function(page) {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/menu-item"]'), function(element) {
+            element.onclick = function() {
                 document.querySelector('#myNavigator').pushPage('html/' + element.getAttribute('data-url'));
             };
         });
     },
 
     //Flat list page
-    flatListPage: function (page) {
+    flatListPage: function(page) {
         let flats = myApp.user.flats();
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/new-flat"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/new-flat"]'), function(element) {
+            element.onclick = function() {
                 document.querySelector('#myNavigator').pushPage('html/flat/flat_new.html');
             };
         });
@@ -71,7 +72,7 @@ myApp.controllers = {
     },
 
     //Single flat page
-    flatPage: function (page) {
+    flatPage: function(page) {
         if (myApp.user.isLandlord()) {
             if ((page.data && Object.keys(page.data).length !== 0) || myApp.user.currentFlat() !== undefined) {
                 let info = myApp.user.currentFlat() ? myApp.user.currentFlat() : page.data.element;
@@ -89,30 +90,31 @@ myApp.controllers = {
             //todo: #display_flat
             //todo: najlepiej stworzyć funkcję w serwisach plik: dashboard.js
 
-            //todo: #edit_flat
-            //todo: wewnątrz funkcji do wyświetlania mieszkania oprogramować guzik edycji
-            //todo: i zapis formularza - można to ogarnąć tak jak w przypadku edycji danych użytkownika - sprawdź userPage
+            myApp.dashboard.editFlat(page)
+                //todo: #edit_flat
+                //todo: wewnątrz funkcji do wyświetlania mieszkania oprogramować guzik edycji
+                //todo: i zapis formularza - można to ogarnąć tak jak w przypadku edycji danych użytkownika - sprawdź userPage
             let flat = ons.createElement('<div>' + info.name + '</div>');
             page.querySelector('.content').appendChild(flat);
         }
     },
 
     //New flat page
-    newFlatPage: function (page) {
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/new-flat"]'), function (element) {
-            element.onclick = function () {
+    newFlatPage: function(page) {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/new-flat"]'), function(element) {
+            element.onclick = function() {
                 document.querySelector('#addFlat').pushPage('html/flat/flat_new.html');
             };
         });
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/add-flat"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/add-flat"]'), function(element) {
+            element.onclick = function() {
                 //TODO check if the name isn't empty string
                 myApp.services.flat.create(page);
             };
         });
     },
 
-    billListPage: function (page) {
+    billListPage: function(page) {
         let bills = myApp.flat.bills();
         if (Object.keys(bills).length === 0) {
             myApp.services.bill.emptyList(page);
@@ -121,15 +123,15 @@ myApp.controllers = {
         }
     },
 
-    billPage: function (page) {
+    billPage: function(page) {
         //todo: display_bill
         //todo: edit_bill
         myApp.services.bill.fill(page, page.data.element);
     },
 
-    usersPage: function (page) {
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/new-tenant"]'), function (element) {
-            element.onclick = function () {
+    usersPage: function(page) {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/new-tenant"]'), function(element) {
+            element.onclick = function() {
                 document.querySelector('#myNavigator').pushPage('html/user/user_new.html');
             };
         });
@@ -142,50 +144,50 @@ myApp.controllers = {
     },
 
     //User info page
-    userPage: function (page) {
+    userPage: function(page) {
         myApp.services.user.fill(page);
 
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/logout"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/logout"]'), function(element) {
+            element.onclick = function() {
                 myApp.services.common.redirectToLogin();
             };
         });
     },
 
-    tenantPage: function (page) {
+    tenantPage: function(page) {
         let info = page.data.element;
         myApp.services.user.fill(page, info);
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/remove-tenant"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/remove-tenant"]'), function(element) {
+            element.onclick = function() {
                 //todo: nie pokazuje się po raz kolejny
                 document.getElementById('removeTenant').show();
             };
         });
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/delete-tenant"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/delete-tenant"]'), function(element) {
+            element.onclick = function() {
                 document.getElementById('removeTenant').hide();
                 ajax.send('post', '/api/user/' + info.id + '/remove', {}, myApp.services.common.updateFlat);
             };
         });
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/cancel"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/cancel"]'), function(element) {
+            element.onclick = function() {
                 document.getElementById('removeTenant').hide();
             };
         });
     },
 
-    tenantNewPage: function (page) {
+    tenantNewPage: function(page) {
         form = $(page.querySelector('form'));
         let action = form.attr('data-ajax').replace('{id}', myApp.user.currentFlatId());
         form.attr('data-ajax', action);
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/save"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/save"]'), function(element) {
+            element.onclick = function() {
                 myApp.services.common.save(page, myApp.services.common.updateFlat);
             };
         });
     },
 
-    dashboardPage: function (page) {
+    dashboardPage: function(page) {
         let lastBill = myApp.flat.bill();
         if (Object.keys(lastBill).length === 0) {
             myApp.services.dashboard.noLastBill(page);
@@ -194,15 +196,15 @@ myApp.controllers = {
         }
     },
 
-    userNoFlatPage: function (page) {
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/logout"]'), function (element) {
-            element.onclick = function () {
+    userNoFlatPage: function(page) {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/logout"]'), function(element) {
+            element.onclick = function() {
                 myApp.services.common.redirectToLogin();
             };
         });
     },
 
-    userAcceptPage: function (page) {
+    userAcceptPage: function(page) {
         let flatInfo = myApp.flat.currentFlat();
         let flat_number = flatInfo.flat_number ? '/' + flatInfo.flat_number : '';
         let flat = ons.createElement(
@@ -210,23 +212,22 @@ myApp.controllers = {
             '<ons-list-item>Ulica i numer: ' + flatInfo.street + ' ' + flatInfo.building_number + flat_number + '</ons-list-item>' +
             '<ons-list-item>Miasto: ' + flatInfo.city + '</ons-list-item>' +
             '</div>'
-            )
-        ;
+        );
 
         page.querySelector('.flat_info').appendChild(flat);
 
         //todo: dopisać wysłanie requesta do api z zaakceptowaniem zaproszenia #accept (/api/accept)
 
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/accept"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/accept"]'), function(element) {
+            element.onclick = function() {
                 console.log('accept');
             };
         });
 
         //todo: dopisać wysłanie requesta do api z odrzuceniem zaproszenia #ignore (/api/ignore)
 
-        Array.prototype.forEach.call(page.querySelectorAll('[component="button/ignore"]'), function (element) {
-            element.onclick = function () {
+        Array.prototype.forEach.call(page.querySelectorAll('[component="button/ignore"]'), function(element) {
+            element.onclick = function() {
                 console.log('no flat');
                 myNavigator.pushPage('html/user/user_no_flat.html');
             };
--- www/assets/js/services/flat.js
+++ www/assets/js/services/flat.js
@@ -2,23 +2,19 @@
 // Flat Service //
 /////////////////
 myApp.services.flat = {
-    list: function (page, flats) {
+    list: function(page, flats) {
         for (let id in flats) {
             let flat = flats[id];
             myApp.services.flat.item(page, flat);
         }
     },
 
-    fill: function (page) {
-        let
-    },
-
-    emptyList: function (page) {
+    emptyList: function(page) {
         let info = ons.createElement('<div>Brak dodanych mieszkań - użyj przycisku by dodać mieszkanie.</div>');
         page.querySelector('.content').appendChild(info);
     },
 
-    item: function (page, flat) {
+    item: function(page, flat) {
         let name = flat.name ? flat.name : flat.street + ' ' + flat.building_number + ', ' + flat.city;
         let flatItem = ons.createElement(
             '<div>' +
@@ -27,7 +23,7 @@ myApp.services.flat = {
         );
 
 
-        flatItem.querySelector('.center').onclick = function () {
+        flatItem.querySelector('.center').onclick = function() {
             // myNavigator.pushPage(myApp.user.splitter() + 'Splitter.html',
             //     {
             //         animation: 'lift',
@@ -41,31 +37,31 @@ myApp.services.flat = {
         page.querySelector('.content').insertBefore(flatItem);
     },
 
-    emptyFlatLandlord: function (page) {
+    emptyFlatLandlord: function(page) {
         let info = ons.createElement('<div>Wybierz lub dodaj mieszkanie.</div>');
         page.querySelector('.content').appendChild(info);
         myApp.services.flat.list(page);
     },
-    create: function (page) {
+    create: function(page) {
         ajax.sendForm(page, myApp.services.flat.onCreatedSuccess(), myApp.services.flat.onCreateFail());
     },
 
 
-    onCreatedSuccess: function () {
+    onCreatedSuccess: function() {
         myNavigator.pushPage('html/flat/flat_info.html');
     },
 
-    onCreateFail: function () {
-        ons.notification.alert({message: 'Nie udalo sie dodac mieszkania!'});
+    onCreateFail: function() {
+        ons.notification.alert({ message: 'Nie udalo sie dodac mieszkania!' });
     },
 
     // Modifies the inner data and current view of an existing flat.
-    update: function (flat, data) {
+    update: function(flat, data) {
 
     },
 
     // Deletes a flat
-    remove: function (taskItem) {
+    remove: function(taskItem) {
 
     }
 };
