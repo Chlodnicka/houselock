452305a9fda2c3f17b7f85b6d6608c9f2b4fe4ac
Chłodnicka <chlodnicka.maja@gmail.com>
2018-05-14 19:53:27 +0200

    config

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -16,3 +16,17 @@ document.addEventListener('init', function (event) {
     }
 
 });
+
+document.addEventListener('deviceready', function () {
+    // Enable to debug issues.
+    window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+    var notificationOpenedCallback = function (jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+
+    window.plugins.OneSignal
+        .startInit("da20d043-c374-4553-8fe4-32a95c7c7742")
+        .handleNotificationOpened(notificationOpenedCallback)
+        .endInit();
+}, false);
