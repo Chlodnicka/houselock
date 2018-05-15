2c76c5703f7b287438568a028a029a373e501a7f
ftancula <ftancula@gmail.com>
2018-05-15 22:38:02 +0200

    new form for editing flat info

--- www/assets/js/services/dashboard.js
+++ www/assets/js/services/dashboard.js
@@ -17,12 +17,37 @@ myApp.services.dashboard = {
         let flat = ons.createElement(
             '<div><ons-list-header>' + info.name + '</ons-list-header>' +
             '<ons-list-item><div>Adres:  </div><div>ul. ' + info.street + ' ' + info.building_number + '/' + info.flat_number + '</div></ons-list-item>' +
+            '<div class="edit" style="display: none">' +
+            '<ons-input id="street" name="street" modifier="underbar" placeholder="Ulica" value="' + info.street + '" float class="edit hidden"> </ons-input>' +
+            '<ons-input id="building_number" name="building_number" modifier="underbar" placeholder="Numer budynku" value="' + info.building_number + '" float class="edit hidden"> </ons-input>' +
+            '<ons-input id="flat_number" name="flat_number" modifier="underbar" placeholder="Numer mieszkania" value="' + info.flat_number + '" float class="edit hidden"> </ons-input>' +
+            '</div>' +
             '<ons-list-item>Miasto i kod pocztowy:  ' + info.city + ' ' + info.postal_code + '</ons-list-item>' +
+            '<div class="edit" style="display: none">' +
+            '<ons-input id="city" name="city" modifier="underbar" placeholder="Miasto" value="' + info.city + '" float class="edit hidden"> </ons-input>' +
+            '<ons-input id="postal_code" name="postal_code" modifier="underbar" placeholder="Kod pocztowy" value="' + info.postal_code + '" float class="edit hidden"> </ons-input>' +
+            '</div>' +
             '<ons-list-item>Kwota za wynajem:  ' + info.mercenary + ' PLN</ons-list-item>' +
+            '<div class="edit" style="display: none">' +
+            '<ons-input id="mercenary" name="mercenary" modifier="underbar" placeholder="Kwota za wynajem" value="' + info.mercenary + '" float class="edit hidden"> </ons-input>' +
+            '</div>' +
             '<ons-list-item>Dzień płatności:  ' + info.pay_day + '</ons-list-item>' +
+            '<div class="edit" style="display: none">' +
+            '<ons-input id="pay_day" name="pay_day" modifier="underbar" placeholder="Dzien płatności" value="' + info.pay_day + '" float class="edit hidden"> </ons-input>' +
+            '</div>' +
+            '<ons-button style="display:none;" modifier="large" component="button/save">Zapisz</ons-button>' +
+            '<ons-button style="display:none;" modifier="large" component="button/cancel">Anuluj</ons-button>' +
             '</div>'
         );
         page.querySelector('.content').appendChild(flat);
+
+        page.querySelector('[component="button/save"]').onclick = function() {
+            myApp.services.flat.save(page)
+        };
+        myApp.services.common.edit(page);
+        myApp.services.common.cancel(page);
+
+
     },
 
 };
